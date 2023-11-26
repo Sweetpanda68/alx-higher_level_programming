8b0c5d16c2dc7e052d801f20ca340867daf6262f
sweetpanda68 <mgirma976@gmail.com>
2023-11-25 21:21:08 -0800

    javascript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
-	console.log('Missing size');
+const x = process.argv[2];
+const change = parseInt(x);
+if (isNaN(change)) {
+  console.log('Missing size');
 } else {
-	for (let r = 0; r < size; r++) {
-		let row = '';
-		for (let c = 0; c < size; c++) row += 'X';
-		console.log(row);
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
 }
