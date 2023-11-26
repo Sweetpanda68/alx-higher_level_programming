f35bf0f805531ebf2802bc5ad8814f8f1b4b0ca6
sweetpanda68 <mgirma976@gmail.com>
2023-11-25 21:08:22 -0800

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-
-// script to print strings using a loop
-
-let i;
-const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (i = 0; i < 3; i++) {
-	console.log(myString[i]);
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
 }
